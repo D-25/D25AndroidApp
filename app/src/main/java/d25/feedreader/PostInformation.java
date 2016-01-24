cf00b41c1c0850251794686157b92a6b171048c2
Danilo Civati <cdanilo25@gmail.com>
2016-01-24 13:40:52 +0100

    Just started.
    This isn't a great change.

---
+++ app/src/main/java/d25/feedreader/PostInformation.java
@@ -0,0 +1,12 @@
+package d25.feedreader;
+
+/**
+ * Created by Danilo on 24/01/2016.
+ */
+public class PostInformation {
+    public String postImageURL;
+    public String postTitle;
+    public String postDesc;
+    public String postDate;
+    public String postAuthor;
+}
